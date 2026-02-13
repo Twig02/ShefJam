afc40903803ad65bbe206b0745c6dfa7fea25c3b
Beth Fisher <null>
2026-02-13 19:05:28 +0000

    Added Canvas and Background, Started on scaling

---
+++ Assets/Scripts/ScreenSizer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ScreenSizer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(
+    }
+}

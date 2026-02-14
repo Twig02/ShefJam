eb975c7c5aa6af87d575c8df4bc9f788246363c6
Ivy <ivyanastasia06@gmail.com>
2026-02-14 15:22:38 +0000

    Fix errors

--- Assets/Scripts/Swipee.cs
+++ Assets/Scripts/Swipee.cs
@@ -1,5 +1,5 @@
-using System.IO;
 using UnityEngine;
+using System.IO;
 
 public class Swipee
 {
@@ -21,15 +21,7 @@ public class Swipee
     protected static string[] jobs = { "Diamond Plug Fixer", "Unemployed", "Pint pourer", "Maccies slave", "Dictator", "Failing musician", "Finance bro", "Uber Eats", "Stripper", "Cereal killer", "Bossman at kebab shop", "Celebrity (on the list)", "Eduroam support", "Politician", "Toilet cleaner", "Lockheed Martin engineer", "Denver Airport baggage handler", "Emo barista", "Uni lecturer", "Student", "Alcoholic", "Evil scientist", "Bouncer", "Drug dealer", "Endcliffe maintenance", "Vietnam veteran", "White van driver", "Fortune teller", "Benefit scrounger", "Eco warrior" };
     protected static string[] datingGoals = { "Shags", "Confused.com", "Gets aftercare", "Wifey", "Greedy" };
 
-    public Swipee(System.Random rnd) 
-    {
-        // Read from the file
-        string path = "Assets/Misc/names.txt";
-
-        StreamReader reader = new StreamReader(path);
-        string allNames = reader.ReadToEnd();
-        reader.Close();
-
+    public Swipee(System.Random rnd) {
         string[] allNames = File.ReadAllLines("names.txt");
         Name = allNames[rnd.Next(allNames.Length)];
         

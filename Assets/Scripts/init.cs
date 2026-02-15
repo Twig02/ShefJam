5527b06f5af3b5c416e426c3687afb06d11dec55
Ivy <ivyanastasia06@gmail.com>
2026-02-15 11:51:45 +0000

    Done for me

--- Assets/Scripts/init.cs
+++ Assets/Scripts/init.cs
@@ -1,25 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI; 
 using System;
 using TMPro;
+using System.Collections.Generic;
 
-public class Init : MonoBehaviour
-{
+ public class Init : MonoBehaviour {
     [SerializeField] private TextMeshProUGUI[] textFields;
+    [SerializeField] private Image profileDisplay;
+    [SerializeField] private List<Sprite> profileSprites = new List<Sprite>();
     public Swipee currentSwipe;
     public Swiper player;
     public int successfulMatches;
     public int successfulDates;
-
-
-    void Start()
-    {
+    void Start() {
         System.Random rmd = new System.Random();
         player = new Swiper(rmd);
         successfulMatches = 0;
         successfulDates = 0;
         currentSwipe = new Swipee(rmd);
         UpdateSwipeScreen(currentSwipe);
-    }
+
+    } 
 
     public void UpdateSwipeScreen(Swipee swipee)
     {
@@ -29,6 +30,6 @@ public class Init : MonoBehaviour
         textFields[3].text = "Purity: " + swipee.RicePurityScore;
         textFields[4].text = "Job:\n" + swipee.Job;
         textFields[5].text = "Dating Intentions:\n" + swipee.DatingIntentions;
-        textFields[6].text = swipee.Description;
+        textFields[6].text = swipee.Description; 
     }
 }
